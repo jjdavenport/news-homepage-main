ee04ecff83522b7dd551cc1e4e67978ed0fadf88
jjdavenport <jordanjamesdavenport@gmail.com>
2024-06-08 21:51:47 +0100

    added background overlay for mobile nav

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 const menuBtn = document.getElementById("menu");
-const nav = document.getElementById("nav");
 const closeBtn = document.getElementById("close");
 
 menuBtn.addEventListener("click", () => {
+  const nav = document.getElementById("nav");
+  const overlay = document.getElementById("overlay");
+  overlay.classList.toggle("overlay-active");
   nav.classList.toggle("nav-active");
+  if (nav.classList.contains("nav-active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "";
+  }
 });
 
 closeBtn.addEventListener("click", () => {
+  const nav = document.getElementById("nav");
+  const overlay = document.getElementById("overlay");
+  overlay.classList.toggle("overlay-active");
   nav.classList.toggle("nav-active");
+  if (nav.classList.contains("nav-active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "";
+  }
 });
 
 window.addEventListener("resize", () => {
+  const nav = document.getElementById("nav");
+  const overlay = document.getElementById("overlay");
   const width = window.innerWidth;
   if (width >= 560) {
     nav.classList.remove("nav-active");
+    overlay.classList.remove("overlay-active");
   }
 });
